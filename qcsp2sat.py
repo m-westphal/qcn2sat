c09f7d068d09a18fd90bfa709e03d050698012f0
Matthias Westphal <westpham@informatik.uni-freiburg.de>
2011-06-07 14:18:29 +0200

    Modify direct encoding to write to stdout

--- qcsp2sat.py
+++ qcsp2sat.py
@@ -7,34 +7,9 @@ import re
 import sys
 import string
 
-def human_readable(i):
-    if i > 1024**3:
-        return str(i/(1024**3))+"Gi"
-    if i > 1024**2:
-        return str(i/(1024**2))+"Mi"
-    if i > 1024*2:
-        return str(i/1024)+"Ki"
-    return str(i)
-
-def progress(idx, total):
-    if idx == 0:
-        sys.stdout.flush()
-
-    if idx+1 == total:
-        print ".",
-        sys.stdout.flush()
-        return
-
-    total_f = float(total)
-    for f in [0.25,0.5,0.75]:
-        if idx == int(f * total_f):
-            print ".",
-            sys.stdout.flush()
-            return
-
 def gen_clause(vs):
     clause = [`v` for v in vs] # turn into strings
-    return string.join(clause+['0\n'])
+    return string.join(clause+['0'])
 
 def readGQRCSP(csp):
     constraints = [ ]
@@ -45,16 +20,16 @@ def readGQRCSP(csp):
         if res:
             x = int(res.group(1))
             y = int(res.group(2))
-            assert x < y
+            assert 0 <= x < y
             rel = res.group(3).strip().split(" ")
             assert rel
-            constraints.append((x,y,rel))
-
+            constraints.append( (x, y, rel) )
     lines.close()
+
     return constraints
 
 def encodeDict(i, j, baserel, dict):
-    br = string.join([str(i),str(j), baserel])
+    br = string.join([str(i), " ", str(j), " ", baserel])
     try:
         return dict[br]
     except KeyError:
@@ -74,7 +49,7 @@ def readCompTable(calculus):
 
     lines = open(calculus, 'r')
     for l in lines:
-        extract = re.search('^(.*):(.*)::.*\((.*)\)$', l)
+        extract = re.search('^(.*):(.*)::[ ]*\((.*)\)$', l)
         assert(extract)
         left = extract.group(1).strip()
         right = extract.group(2).strip()
@@ -88,94 +63,70 @@ def readCompTable(calculus):
 
     return table, frozenset(ALL_RELATIONS)
 
-def full_qcsp(constraints, ALL_RELATIONS):
+def full_qcsp(constraints, ALL_RELATIONS):  # turn the CSP into a complete constraint network
     max_node = max( [ t for (_, t, _) in constraints ] )
     CSP = dict()
+    for i in xrange(0, max_node+1):
+        CSP[i] = dict()
+        for j in range(i+1, max_node+1):
+            CSP[i][j] = ALL_RELATIONS
 
-    defined_pairs = [ ] # used (explicit) constraint scopes
     for i, j, r in constraints:
         assert i < j
         assert j <= max_node
-
-        CSP[i*(max_node+1)+j] = r
-
-    for i in range(max_node+1):
-        for j in range(i+1, max_node+1):
-            key = i*(max_node+1)+j
-            if key not in CSP.keys():
-                CSP[key] = ALL_RELATIONS
+        CSP[i][j] = r
 
     return max_node, CSP
 
-def writeSATdirect(fd, constraints, calculus):
-    print "Read qualitative composition table:",
+def writeSATdirect(constraints, calculus):
     comptable, ALL_RELATIONS = readCompTable(calculus)
-    print "done (%d base relation)" % len(ALL_RELATIONS)
 
     max_node, CSP = full_qcsp(constraints, ALL_RELATIONS)
 
-    dict = { }
-    cnf_bytes = 0
-
-    print "Preparing dictionary and data structures:",
-    for i in range(max_node+1):
-        for j in range(i+1, max_node+1):
-            for br in CSP[i*(max_node+1)+j]:
-                encodeDict(i, j, br, dict)
-        progress(i, max_node)
-    print "done (allocated %d boolean variables)" % dict["max"]
-
+    boolvars = { } # maps b in R_ij to boolean variable (direct encoding)
     clauses = [ ]
-    print "Construct ALO, AMO clauses:",
+
+#    print "Construct ALO, AMO clauses:",
     for i in xrange(max_node+1):
         for j in xrange(i+1, max_node+1):
-            r = CSP[i*(max_node+1)+j]
+            r = CSP[i][j]
             # ALO
-            clause = gen_clause([ encodeDict(i, j, br, dict) for br in r])
-            cnf_bytes += len(clause)
+            clause = gen_clause([ encodeDict(i, j, br, boolvars) for br in r])
             clauses.append(clause)
 
             # AMO
             for br in r:
                 for br2 in r:
                     if br < br2:
-                        clause = gen_clause([ -1 * encodeDict(i, j, br, dict), -1 * encodeDict(i, j, br2, dict) ])
-                        cnf_bytes += len(clause)
+                        clause = gen_clause([ -1 * encodeDict(i, j, br, boolvars), -1 * encodeDict(i, j, br2, boolvars) ])
                         clauses.append(clause)
                     else:
                         assert br == br2 or br2 < br
-        progress(i, max_node)
-    print "done (%s bytes prepared)" % human_readable(cnf_bytes)
 
-    # SUPPORT
-    print "Construct support clauses (cubic time/space):",
+#    print "Construct support clauses (cubic time/space):",
     for i in xrange(max_node+1):
         for j in xrange(i+1, max_node+1):
-            r_ij = CSP[i*(max_node+1)+j]
+            r_ij = CSP[i][j]
 
             for k in xrange(j+1, max_node+1):
-                r_ik = CSP[i*(max_node+1)+k]
-                r_jk = CSP[j*(max_node+1)+k]
+                r_ik = CSP[i][k]
+                r_jk = CSP[j][k]
                 for br1 in r_ij:
-                    not_b_ij = -1 * encodeDict(i, j, br1, dict)
+                    not_b_ij = -1 * encodeDict(i, j, br1, boolvars)
                     for br2 in r_jk:
                         intersection = frozenset(r_ik) & comptable[br1 + " " + br2]
 
-                        cl = [ not_b_ij, -1 * encodeDict(j, k, br2, dict) ]
-                        cl += [ encodeDict(i, k, br, dict) for br in intersection ]
+                        cl = [ not_b_ij, -1 * encodeDict(j, k, br2, boolvars) ]
+                        cl += [ encodeDict(i, k, br, boolvars) for br in intersection ]
                         clause = gen_clause(cl)
-                        cnf_bytes += len(clause)
                         clauses.append(clause)
-        progress(i, max_node)
-    print "done (%s bytes prepared)" % human_readable(cnf_bytes)
 
-    header = "p cnf %d %d\n" % ( dict["max"], len(clauses))
-    cnf_bytes += len(header)
-    print "All done! Writing SAT instance (%s bytes)." % human_readable(cnf_bytes)
-    print "\t-> %d variables, %d clauses" % (dict["max"], len(clauses))
-    fd.write(header)
+    header = "p cnf %d %d\n" % ( boolvars["max"], len(clauses))
+#    print "All done! Writing SAT instance (%s bytes)." % human_readable(cnf_bytes)
+#    print "\t-> %d variables, %d clauses" % (dict["max"], len(clauses))
+    print header
     for c in clauses:
-        fd.write(c + "\n")
+        print c
 
 def writeSATgac(fd, constraints, calculus):
     print "Read qualitative composition table:",
@@ -247,18 +198,18 @@ def writeSATgac(fd, constraints, calculus):
 
     header = "p cnf %d %d\n" % ( dict["max"], len(clauses))
     cnf_bytes += len(header)
-    print "All done! Writing SAT instance (%s bytes)." % human_readable(cnf_bytes)
-    print "\t-> %d variables, %d clauses" % (dict["max"], len(clauses))
+#    print "All done! Writing SAT instance (%s bytes)." % human_readable(cnf_bytes)
+#    print "\t-> %d variables, %d clauses" % (dict["max"], len(clauses))
     fd.write(header)
     for c in clauses:
         fd.write(c + "\n")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        raise SystemExit("Usage: <script> GQR_COMPOSITION_TABLE_FILE GQR_QCSP OUTPUTFILE")
+    if len(sys.argv) != 3:
+        raise SystemExit("Usage: <script> GQR_COMPOSITION_TABLE_FILE GQR_QCSP")
 
+    composition_table = sys.argv[1]
     qcsp = readGQRCSP(sys.argv[2])
-    print "Loaded QCSP with", max([ t for (_, t, _) in qcsp])+1, "qualitative variables"
+#    print "Loaded QCSP with", max([ t for (_, t, _) in qcsp])+1, "qualitative variables"
 
-    outfile = open(sys.argv[3], 'w')
-    writeSATdirect(outfile, qcsp, sys.argv[1])
+    writeSATdirect(qcsp, composition_table)
