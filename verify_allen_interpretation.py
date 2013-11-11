149ea0e66a1324c81130c6072a843d9fec7b8d3e
Matthias Westphal <westpham@informatik.uni-freiburg.de>
2013-11-11 09:20:14 +0100

    pylint: verifier

--- verify_allen_interpretation.py
+++ verify_allen_interpretation.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
+"""Internal test script to run some test on .map files. Mostly undocumented."""
 
 # ex: set tabstop=4 expandtab softtabstop=4:
 
@@ -19,10 +20,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-allen_signature = [ '=', '<', '>', 's', 'si', 'f', 'fi', 'd', 'di', 'm', 'mi', 'o', 'oi' ]
+ALLEN_SIGNATURE = [ '=', '<', '>', 's', 'si', 'f', 'fi', 'd', 'di', 'm',
+    'mi', 'o', 'oi' ]
 
 # description of intervals (x,y) in terms of start (?,-) and endpoints (?,+)
-full_point_description = {
+FULL_POINT_DESCRIPTION = {
     '=': ['=--', '<-+', '>+-', '=++'],
     '<': ['<--', '<-+', '<+-', '<++'],
     '>': ['>--', '>-+', '>+-', '>++'],
@@ -38,16 +40,19 @@ full_point_description = {
     'fi': ['<--', '<-+', '>+-', '=++']
 }
 
-def is_allen_interpretation(syn_map):
+def is_allen_interpretation(syntactic_map):
+    """Check input signature of syntactic map."""
     symbols = set()
-    for relation in syn_map:
+    for relation in syntactic_map:
         for base in relation:
             symbols.add(base)
 
-    if symbols != frozenset(allen_signature):
+    if symbols != frozenset(ALLEN_SIGNATURE):
         raise SystemExit('Given signature does not match Allen signature')
 
 def evaluate_predicate(atom):
+    """Return Allen base relations satisfied by given atom."""
+
     sig_1 = atom.var1[1]
     predicate = atom.relation
     sig_2 = atom.var2[1]
@@ -70,24 +75,26 @@ def evaluate_predicate(atom):
         compare = [ '>', '=' ]
 
     relations = set()
-    for br in allen_signature:
-        for prop in full_point_description[br]:
+    for base_relation in ALLEN_SIGNATURE:
+        for prop in FULL_POINT_DESCRIPTION[base_relation]:
             if prop[1:] != sig_1+sig_2:
                 continue
             for base in compare:
                 assert base in [ '<', '=', '>' ]
                 if base == prop[0]:
-                    relations.add(br)
+                    relations.add(base_relation)
 
     return relations
 
-def evaluate_atom(atom):
-    relations = evaluate_predicate(atom[1])
-    if atom[0]:
+def evaluate_atom(literal):
+    """Return Allen base relations satisfying given literal."""
+    relations = evaluate_predicate(literal[1])
+    if literal[0]:
         return relations
-    return set(allen_signature) ^ relations
+    return set(ALLEN_SIGNATURE) ^ relations
 
 def evaluate_clause(clause):
+    """Return Allen base relations satisfying given clause."""
     relations = set()
     for atom in clause:
         relations |= evaluate_atom(atom)
@@ -95,43 +102,45 @@ def evaluate_clause(clause):
     return relations
 
 def evaluate_cnf(cnf):
-    relations = set(allen_signature)
+    """Return Allen base relations satisfying given CNF."""
+    relations = set(ALLEN_SIGNATURE)
     for clause in cnf:
         relations &= evaluate_clause(clause)
 
     return relations
 
-def verify_is_fo_interpretation(syn_map):
+def verify_is_fo_interpretation(syntactic_map):
+    """Test all defining formulas in syntactic map."""
     is_valid = True
-    for relation in syn_map:
-        relations = evaluate_cnf(syn_map[relation])
+    for relation in syntactic_map:
+        relations = evaluate_cnf(syntactic_map[relation])
 
         if relation != relations:
             print "Defining formula for %s broken" % relation
-            print syn_map[relation]
+            print syntactic_map[relation]
             print "yields"
             print relations
             print
             is_valid = False
     return is_valid
 
-def verify_minimality(syn_map):
-    """Non-redundant list of prime implicates"""
+def verify_minimality(syntactic_map):
+    """Test non-redundant list of prime implicates."""
     from copy import deepcopy
 
-    for r in syn_map:
-        def_formula = syn_map[r]
+    for relation in syntactic_map:
+        def_formula = syntactic_map[relation]
 
-        for clause in syn_map[r]:
+        for clause in syntactic_map[relation]:
             if len(clause) == 1:
                 continue
             for atom in clause:
                 mod_clause = set(clause)
                 mod_clause.remove(atom)
-                if r <= evaluate_clause(mod_clause):
+                if relation <= evaluate_clause(mod_clause):
                     print "Defining formula for %s broken" % relation
                     print "Formula is"
-                    print syn_map[r]
+                    print syntactic_map[relation]
                     print "Clause"
                     print clause
                     print "is however not a PRIME implicate."
@@ -139,12 +148,12 @@ def verify_minimality(syn_map):
                     return False
 
         mod_formula = None
-        for clause in syn_map[r]:
+        for clause in syntactic_map[relation]:
             mod_formula = deepcopy(def_formula)
             mod_formula.remove(clause)
 
-            if r == evaluate_cnf(mod_formula):
-                print "Relation '%s' is not minimally defined:" % r
+            if relation == evaluate_cnf(mod_formula):
+                print "Relation '%s' is not minimally defined:" % relation
                 print
                 print "Original defintion"
                 print def_formula
@@ -159,26 +168,28 @@ def verify_minimality(syn_map):
     return True
 
 if __name__ == '__main__':
-    print "Script for verifying syntactic maps of Allen's Interval Calculus"
-    print
-    print "Usage: scrip <some.map>"
-
     from sys import argv
-    map_file = argv[1]
-    print "Read '%s'" % (map_file)
+
+    if len(argv) != 2:
+        print "Script for verifying syntactic maps of Allen's Interval Calculus"
+        print
+        print "Usage: scrip <some.map>"
+
+    MAP_FILE = argv[1]
+    print "Read '%s'" % (MAP_FILE)
     from syntactic_map import read_map
-    syn_map = read_map(map_file)
+    SYNTACTIC_MAP = read_map(MAP_FILE)
     print "DONE"
 
-    is_allen_interpretation(syn_map)
+    is_allen_interpretation(SYNTACTIC_MAP)
 
     print
     print "Verify map is a FO interpretation"
-    if not verify_is_fo_interpretation(syn_map):
+    if not verify_is_fo_interpretation(SYNTACTIC_MAP):
         raise SystemExit("Aborting script. Fix map first to run further tests.")
     print "... is valid"
 
     print
     print "Verify minimality of defining formulas"
-    if verify_minimality(syn_map):
+    if verify_minimality(SYNTACTIC_MAP):
         print "... act as prime implicates."
